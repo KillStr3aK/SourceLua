d16c215e02b57163a73b0cc070ec61ae98d49b61
Nexd <statser07@gmail.com>
2023-03-20 01:05:12 +0100

    Add files via upload

---
+++ extension.cpp
@@ -0,0 +1,15 @@
+#include "SDK/shared.h"
+
+SourceLua SourceLua;
+SMEXT_LINK(&SourceLua);
+
+bool SourceLua::SDK_OnLoad(char* error, size_t maxlength, bool late)
+{
+    LuaScripting::Initialize();
+    return true;
+}
+
+void SourceLua::SDK_OnUnload(void)
+{
+    LuaScripting::Release();
+}
